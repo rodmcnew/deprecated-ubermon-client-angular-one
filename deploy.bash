b305e99fc0fa85265dd401f889e0021772c63829
rodmcnew <rodmcnew@gmail.com>
2017-01-24 13:06:30 -0600

    add build files

--- deploy.bash
+++ deploy.bash
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-npm run build
 git remote add production git@github.com:ubermon-client-angular-one/ubermon-client-angular-one.github.io.git
 git add . && git commit -m "add build files"
 git push origin master
